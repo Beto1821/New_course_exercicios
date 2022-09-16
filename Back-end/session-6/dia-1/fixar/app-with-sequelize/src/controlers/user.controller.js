846af912dfc7bcee0e529f0036f2fb7a4b093fa2
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-09-16 13:43:59 -0300

    sequelize fixar

--- Back-end/session-6/dia-1/fixar/app-with-sequelize/src/controlers/user.controller.js
+++ Back-end/session-6/dia-1/fixar/app-with-sequelize/src/controlers/user.controller.js
@@ -12,41 +12,72 @@ const getAll = async (_req, res) => {
   }
 };
 
-const getById = async (id) => {
-  const user = await User.findByPk(id);
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await UserService.getById(id);
+  
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
 
-  return user;
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
 };
 
-const getByIdAndEmail = async (id, email) => {
-  const user = await User.findOne({ where: { id, email } });
+const getByIdAndEmail = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { email } = req.query;
+    const user = await UserService.getByIdAndEmail(id, email);
+  
+    if (!user) return res.status(404).json({ message: 'Usuário não encontrado' });
 
-  return user;
+    return res.status(200).json(user);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
 };
 
-const createUser = async (fullName, email) => {
-  const newUser = await User.create({ fullName, email });
+const createUser = async (req, res) => {
+  try {
+    const { fullName, email } = req.body;
+    const newUser = await UserService.createUser(fullName, email);
 
-  return newUser;
+    return res.status(201).json(newUser);
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
 };
 
-const updateUser = async (id, fullName, email) => {
-  const [updatedUser] = await User.update(
-    { fullName, email },
-    { where: { id } },
-  );
+const updateUser = async (req, res) => {
+  try {
+    const { fullName, email } = req.body;
+    const { id } = req.params;
+    const updatedUser = await UserService.updateUser(id, fullName, email);
 
-  console.log(updatedUser); // confira o que é retornado quando o user com o id é ou não encontrado;  
-  return updatedUser;
+    if (!updatedUser) return res.status(404).json({ message: 'Usuário não encontrado' });
+
+    return res.status(200).json({ message: 'Usuário atualizado com sucesso!' });    
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
 };
 
-const deleteUser = async (id) => {
-  const user = await User.destroy(
-    { where: { id } },
-  );
+const deleteUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+    await UserService.deleteUser(id);
 
-  console.log(user); // confira o que é retornado quando o user com o id é ou não encontrado;
-  return user;
+    return res.status(200).json({ message: 'Usuário excluído com sucesso!' });
+  } catch (e) {
+    console.log(e.message);
+    res.status(500).json({ message: error500Message });
+  }
 };
 
 module.exports = {

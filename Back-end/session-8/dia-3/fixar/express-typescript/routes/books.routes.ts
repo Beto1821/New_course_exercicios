94c017deeeb4b95ffc3c55055be77ba35b538c16
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-10-05 16:05:11 -0300

    fixar 8.3

--- Back-end/session-8/dia-3/fixar/express-typescript/routes/books.routes.ts
+++ Back-end/session-8/dia-3/fixar/express-typescript/routes/books.routes.ts
@@ -11,6 +11,8 @@ const booksController = new BooksController();
 router.get('/books', booksController.getAll);
 router.get('/books/:id', booksController.getById);
 router.post('/books/', validationBook, booksController.create);
+router.put('/books/:id', validationBook, booksController.update);
+router.delete('/books/:id', validationBook, booksController.remove);
 
 
 export default router;

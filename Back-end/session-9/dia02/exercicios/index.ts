f11af9fa1ed667bfb695e53dafa0419bd411fcb1
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-10-13 01:01:42 -0300

    eercicio 27.2

---
+++ Back-end/session-9/dia02/exercicios/index.ts
@@ -0,0 +1,11 @@
+import Person from './Person';
+
+const maria = new Person('Maria da Consoloção', new Date('1980/01/25'));
+const luiza = new Person('Luiza Andrade', new Date('2005/10/20'));
+
+console.log(maria);
+console.log(luiza);
+
+
+
+

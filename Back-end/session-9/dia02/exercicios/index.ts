b37c97c6c412aec7bf5fc384142a286c9b3fb229
Adalberto R. Ribeiro <beto1821@uol.com.br>
2022-10-14 11:19:56 -0300

    session9 req5

--- Back-end/session-9/dia02/exercicios/index.ts
+++ Back-end/session-9/dia02/exercicios/index.ts
@@ -1,12 +1,12 @@
 
 // exer)1
-import Person from './Person';
+// import Person from './Person';
 
-const maria = new Person('Maria da Consoloção', new Date('1980/01/25'));
-const luiza = new Person('Luiza Andrade', new Date('2005/10/20'));
+// const maria = new Person('Maria da Consoloção', new Date('1980/01/25'));
+// const luiza = new Person('Luiza Andrade', new Date('2005/10/20'));
 
-console.log(maria);
-console.log(luiza);
+// console.log(maria);
+// console.log(luiza);
 
 // Exer02
 
@@ -68,3 +68,26 @@ console.log(luiza);
 
 // Exerc05
 
+import Subject from './Subject';
+import Teacher from './Teacher';
+
+const math = new Subject('Matemática');
+const history = new Subject('História');
+const philosophy = new Subject('Filosofia');
+
+const marta = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+const joao = new Teacher('João Antônio da Costa', new Date('1982/04/21'), 2000, history);
+const lucio = new Teacher('Lucio Teixeira', new Date('1986/01/29'), 2000, philosophy);
+
+console.log(marta);
+console.log(joao);
+console.log(lucio);
+
+// deve retornar erro
+//const invalidTeacherSalary = new Teacher('Marta da Silva', new Date('1980/03/30'), 2000, math);
+
+// deve retornar erro
+// const today = new Date();
+// today.setDate(today.getDate() + 1)
+// const tomorrow = today;
+// marta.admissionDate = tomorrow;

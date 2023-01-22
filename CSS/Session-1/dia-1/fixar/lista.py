26decde6b76abd1fa7b82b83899d21cd216f78b0
Adalberto R. Ribeiro <beto1821@uol.com.br>
2023-01-21 22:57:12 -0300

    fixar1.1

---
+++ CSS/Session-1/dia-1/fixar/lista.py
@@ -0,0 +1,13 @@
+# Exercício 5: Adicione o elemento “Ciência da Computação” à lista.
+
+# Exercício 6: Acesse e altere o primeiro elemento da lista para “Fundamentos”.
+
+trybe_course = []
+
+trybe_course.append("Ciência da Computação")
+
+print(trybe_course)
+
+trybe_course[0] = "Fundamentos"
+
+print(trybe_course)
